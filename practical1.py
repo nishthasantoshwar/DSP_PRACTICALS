f42603f7e00e08f91721af8d33d9ab6087ba446c
nishthasantoshwar <santoshwarnv@jdcoem.ac.in>
2026-09-06 19:37:54 +0530

    practical1.py

---
+++ practical1.py
@@ -0,0 +1,35 @@
+# 1. Simple If
+x = 10
+if x > 5:
+    print("Greater than 5")
+
+
+# 2. If-Else
+x = 3
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# 3. If-Elif-Else
+marks = 75
+if marks >= 90:
+    print("A Grade")
+elif marks >= 60:
+    print("B Grade")
+else:
+    print("C Grade")
+
+
+# 4. Loops
+
+# For Loop
+for i in range(5):
+    print(i)
+
+# While Loop
+i = 0
+while i < 5:
+    print(i)
+    i += 1
